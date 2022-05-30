a04bc517533ecf3025c6fdc4a7f95f9d5704530b
oussama <abz1999abz@gmail.com>
2022-05-30 23:13:07 +0100

    adding faker impl

--- src/main/java/com/indatacore/demo/controller/CSVController.java
+++ src/main/java/com/indatacore/demo/controller/CSVController.java
@@ -2,8 +2,12 @@ package com.indatacore.demo.controller;
 
 import com.indatacore.demo.csv.BufferReader;
 import com.indatacore.demo.model.CsvRow;
+import com.indatacore.demo.service.CsvRowService;
+import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
+import net.datafaker.fileformats.Csv;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +15,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/csv")
+@AllArgsConstructor
 public class CSVController {
+    private final CsvRowService service;
+
     @SneakyThrows
     @GetMapping("/")
     List<CsvRow> getData(){
         return BufferReader.getRecords();
+    }
+
+    @PostMapping("/")
+    CsvRow addRandomRow(){
+        return service.addRandomRow();
     }
 }
--- src/main/java/com/indatacore/demo/service/CsvRowServiceImpl.java
+++ src/main/java/com/indatacore/demo/service/CsvRowServiceImpl.java
@@ -1,10 +1,13 @@
 package com.indatacore.demo.service;
 
 import com.indatacore.demo.model.CsvRow;
+import com.indatacore.demo.model.Gender;
 import com.indatacore.demo.repository.CsvRowRepository;
 import lombok.AllArgsConstructor;
+import net.datafaker.Faker;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -24,6 +27,13 @@ public class CsvRowServiceImpl implements CsvRowService{
 
     @Override
     public CsvRow addRandomRow() {
-        return null;
+        Random random = new Random();
+        Faker faker = new Faker();
+        CsvRow row = new CsvRow();
+        row.setId(UUID.randomUUID());
+        row.setName(faker.name().fullName());
+        row.setWeeks(300 + random.nextInt(100));
+        row.setGender(random.nextInt(10) % 2 == 0? Gender.MALE : Gender.FEMALE);
+        return repository.save(row);
     }
 }
